f6c7bc8c08c46760a32cdafd5c05b573f28145cf
RodrigoSandri <Guigosandri@gmail.com>
2024-09-12 21:19:06 -0300

    criei as propriedades e ralizei o construtor da classe matriz

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import Matrizes.Matriz;
 
 public class Main {
     public static void main(String[] args)
     {
+        Matriz matriz1 = new Matriz(4, 5 );
+        
 
 
 
--- src/Matrizes/Matriz.java
+++ src/Matrizes/Matriz.java
@@ -1,4 +1,13 @@
 package Matrizes;
 
 public class Matriz {
+    private int[][] matriz;
+    private int linha;
+    private int coluna;
+
+    public Matriz(int linha, int coluna){
+        this.linha = linha;
+        this.coluna = coluna;
+        this.matriz = new int[linha][coluna];
+    }
 }
