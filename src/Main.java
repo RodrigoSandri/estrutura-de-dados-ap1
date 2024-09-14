1380169b06184c125709fbb210e4f1b757d6134e
RodrigoSandri <Guigosandri@gmail.com>
2024-09-13 22:31:29 -0300

    implemebtei funcoes na classe matriz

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,19 @@ import Matrizes.Matriz;
 public class Main {
     public static void main(String[] args)
     {
-        Matriz matriz1 = new Matriz(4, 5 );
+        Matriz matriz1 = new Matriz(7, 9);
+        Matriz matriz2 = new Matriz(7, 8);
+        System.out.println("Primeira matriz original");
+        matriz1.exibirMatriz();
 
-        matriz1.preencherMatriz();
+        System.out.println("Inserindo valor na posição");
+        matriz1.inserirElemento(0, 4, 7);
 
-        System.out.println("Teste");
+        System.out.println("Valor inserido");
+        matriz1.exibirMatriz();
+
+
+//        System.out.println("Segunda matriz original");
+//        matriz2.exibirMatriz();
     }
 }
--- src/Matrizes/Matriz.java
+++ src/Matrizes/Matriz.java
@@ -1,5 +1,7 @@
 package Matrizes;
 
+import java.util.Random;
+
 public class Matriz {
     private int[][] matriz;
     private int linha;
@@ -12,6 +14,46 @@ public class Matriz {
     }
 
     public void preencherMatriz(){
-        System.out.println("teste  asdasd");
+        Random random = new Random();
+        for (int a = 0; a < linha; a++){
+            for (int l = 0; l < coluna; l++){
+                this.matriz[a][l] = random.nextInt(100);
+            }
+        }
+    }
+
+    public void inserirElemento(int l, int c, int valor){
+        if (l != 0)
+            linha = l--;
+        if (c != 0)
+            coluna = c--;
+//        if (linha >= 0 && this.linha < linha && coluna >= 0 && this.coluna < coluna){
+        this.matriz[linha][coluna] = valor;
+//        } else {
+//            System.out.println("Posição inválidade!!!");
+//        }
+    }
+
+    public void removerElemento(int linha, int coluna){
+        if (linha >= 0 && this.linha < linha && coluna >= 0 && this.coluna < coluna){
+            this.matriz[linha][coluna] = 0;
+        } else {
+            System.out.println("Posição inválidade!!!");
+        }
+
+    }
+
+    public void exibirMatriz() {
+        for (int i = 0; i < linha; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < coluna; j++) {
+                System.out.printf("%4d ", matriz[i][j]);
+            }
+            System.out.println("|");
+        }
+    }
+
+    public int[][] getMatriz(){
+        return matriz;
     }
 }
